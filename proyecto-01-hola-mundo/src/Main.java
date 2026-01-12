d2adb0942fef2baea622ad488f21aeb03b0f2609
Frank-107 <20253ds107@utez.edu.mx>
2026-01-12 17:19:14 -0600

    primer captura trabajo hola mundo

---
+++ proyecto-01-hola-mundo/src/Main.java
@@ -0,0 +1,7 @@
+public class Main{
+    static void main(String[] args) {
+        System.out.println("Hola mundo, primer trabajo");
+        System.out.println("Francisco Emmanuel Fuentes Pérez 12/01/2026 2D DSM");
+
+    }
+}
