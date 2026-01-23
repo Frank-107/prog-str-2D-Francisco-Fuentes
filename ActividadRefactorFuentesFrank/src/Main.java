89518eef0eab3bd82c62f8c71d9b80f3e481c5b8
frank-107 <20253ds107@utez.edu.mx>
2026-01-22 22:39:33 -0600

    trabajo opcional

--- ActividadRefactorFuentesFrank/src/Main.java
+++ ActividadRefactorFuentesFrank/src/Main.java
@@ -1,20 +1,20 @@
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        Scanner leer = new scanner(System.in);
-        System.out.print("a: ");
-        int num_a = pedir_entero(scanner);
-        System.out.print("b: ");
-        int b = leer.nextInt();
-        System.out.print("c: ");
-        int c = leer.nextInt();
+        Scanner leer = new Scanner(System.in);
+
+
+        int a = pedir_entero(leer,"hola, dame el numero 1");
+        int b = pedir_entero(leer,"dame el numero 2");
+        int c = pedir_entero(leer, "dame el numero 3");
 
         int suma = a + b + c;
         System.out.println("suma=" + suma);
         System.out.println("prom=" + (suma / 3.0));
     }
-    public static int pedir_entero (scanner leer){
-        System.out.println("Dame un numero");
-        int numero = leer.nextInt();
-        return numero;
+    public static int pedir_entero (Scanner leer, String mensaje){
+        System.out.println(mensaje);
+        return leer.nextInt();
     }
+
 }
