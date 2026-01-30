9f26bfe55488da792599d5561bc14f529271b3cc
Frank-107 <20253ds107@utez.edu.mx>
2026-01-30 09:34:07 -0600

    avance tarea 1 30 ene

---
+++ MenuDeConversionFran/src/Main.java
@@ -0,0 +1,79 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args){
+        int opcion = 0;
+        boolean condicion = false;
+        Scanner leer = new Scanner(System.in);
+        while (true){
+        do {
+            System.out.println("¿Que conversion desea hacer?");
+            System.out.println(" 1) C a F\n 2) F a C\n 3) Km a Millas\n 4) Millas a Km \n 5) salir");
+            opcion = leer.nextInt();
+            if (opcion > 5 || opcion <= 0){
+                System.out.println("Opcion no valida");
+                condicion = false;
+            }
+            else {
+                condicion = true;
+            }
+        } while (!condicion);
+        switch (opcion) {
+            case 1:
+                System.out.println("la conversion es: "+ caf(leer)+ " F");
+                break;
+            case 2:
+                System.out.println("la conversion es: "+ fac(leer)+" C");
+                break;
+            case 3:
+                System.out.println("la conversion es: "+ kam(leer)+ " Millas");
+                break;
+            case 4:
+                System.out.println("la conversion es: "+ mak(leer)+ " Km");
+                break;
+            case 5 :
+                System.out.println("Selecionaste salir");
+                return;
+        }}
+
+
+    }
+    public static double caf(Scanner leer){
+        double variable = 0;
+        boolean numerico = false;
+        System.out.println("Opcion elegida: C a F");
+        System.out.println("¿Cual es el valor?");
+        do {
+            if (leer.hasNextInt()){
+                variable = leer.nextDouble();
+                numerico=true;
+                leer.next();
+            }
+            else{
+                System.out.println("El valor ingresado no es numerico");
+            }
+        }
+        while(numerico);
+        return (variable*(9/5))+32;
+    }
+    public static double fac(Scanner leer){
+        double variable = 0;
+        System.out.println("Opcion elegida: F a C");
+        System.out.println("¿Cual es el valor?");
+        variable = leer.nextDouble();
+        return (variable-32)*(5/9);
+    }
+    public static double kam(Scanner leer){
+        double variable = 0;
+        System.out.println("Opcion elegida: Km a Millas");
+        System.out.println("¿Cual es el valor?");
+        variable = leer.nextDouble();
+        return (variable*0.621371);
+    }
+    public static double mak(Scanner leer){
+        double variable = 0;
+        System.out.println("Opcion elegida: Millas a Km");
+        System.out.println("¿Cual es el valor?");
+        variable = leer.nextDouble();
+        return (variable*1.60934);
+    }
+}
