0efaf68b6d96173e8479f00137a1d2c06b22b788
frank-107 <20253ds107@utez.edu.mx>
2026-02-06 17:53:39 -0600

    cotizador de envios terminado

--- CotizadorDeEnvios/src/Main.java
+++ CotizadorDeEnvios/src/Main.java
@@ -9,16 +9,20 @@ class Main{
         //creacion de objetos
         Scanner leer = new Scanner(System.in);
         InputDataVal inputDataVal = new InputDataVal();
+        ShoppingCalculator shoppingCalculator = new ShoppingCalculator();
 
         //input
         double pesoKg = inputDataVal.getValidDouble(leer, "Dame el peso en Kg (Rango = 0.1-50) ",50,0.1);
         int distanciaKm = inputDataVal.getValidInt(leer,"Dame los Km (Rango = 1-2000)","Fuera de rango",1,2000);
         int tipoServicio = inputDataVal.getValidInt(leer, "Seleccione el tipo de servicio (Estandar=1 Express=2)","Opcion no valida",1,2);
         boolean zonaRemota = inputDataVal.getValidBoolean(leer);
-        System.out.println(zonaRemota);
 
         //process
-        double total ;
+        shoppingCalculator.proceso(pesoKg,distanciaKm,tipoServicio,zonaRemota);
+
+        //output
+        shoppingCalculator.imprimirTicket(tipoServicio,pesoKg,distanciaKm,zonaRemota);
+
     }
 
 
--- CotizadorDeEnvios/src/ShoppingCalculator.java
+++ CotizadorDeEnvios/src/ShoppingCalculator.java
@@ -1,6 +1,14 @@
 public class ShoppingCalculator {
     public final double IVA = 0.16;
-    public double calcularSubtotal(double peso, int distancia, int tipo, boolean zona){
+    public double subtotal;
+    public double iva;
+    public double total;
+    public void proceso(double peso, int distancia, int tipo, boolean zona){
+        calcularSubtotal(peso,distancia,tipo,zona);
+        calcularIVA(this.subtotal);
+        calcularTotal(this.subtotal,this.iva);
+    }
+    private void calcularSubtotal(double peso, int distancia, int tipo, boolean zona){
         double base;
         double precioPorPeso = peso*12;
         double precioPorDistancia;
@@ -22,14 +30,33 @@ public class ShoppingCalculator {
         }else{
             subtotal = base+precioPorDistancia+precioPorPeso;
         }
-        return subtotal;
+        this.subtotal= subtotal;
 
     }
-    public double calcularIVA(double subtotal){
-        return subtotal*IVA;
+    private void calcularIVA(double subtotal){
+        this.iva = subtotal*IVA;
     }
-    public double calcularTotal(double subtotal, double IVA){
-        return subtotal+IVA;
+    private void calcularTotal(double subtotal, double IVA){
+        this.total = subtotal+IVA;
+    }
+
+    public void imprimirTicket(int servicio, double peso, int distancia, boolean zona){
+        System.out.print("El tipo de servicio fue el: ");
+        if (servicio==1) {
+            System.out.println("estandar");
+        }else{
+            System.out.println("express");
+        }
+        System.out.println("El peso fue de "+peso+ " kg");
+        System.out.println("La distancia fue de "+distancia+" km");
+        if (zona){
+            System.out.println("Se encontraba en zona remota");
+        }else{
+            System.out.println("No se encontraba en zona remota");
+        }
+        System.out.println("Su subtotal fue: "+this.subtotal);
+        System.out.println("El iva fue de: "+this.iva);
+        System.out.println("El total sera de: "+this.total);
     }
 
 
