e067f3ea944951a7c6d9b2bb2e1717ff988e2bc2
Frank-107 <20253ds107@utez.edu.mx>
2026-02-13 08:23:08 -0600

    herencia y polinoseque

---
+++ HerenciaYPoliformismo/src/Main.java
@@ -0,0 +1,32 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        Perro fido = new Perro("fido");
+        Gato raul = new Gato("Raul");
+
+        fido.comer();
+        fido.hacerSonido();
+
+        raul.comer();
+        raul.hacerSonido();
+        System.out.println("------------");
+
+        Animal[] animales = new Animal[3];
+        animales[0] = new Perro("Ramonchis");
+        animales[1] = new Gato("Salem");
+        animales[2] = new Perro("firulais");
+        for (Animal animal: animales){
+            animal.hacerSonido();
+        }
+        System.out.println("----------");
+        Animal miercoles = new Perro("Miercoles");
+
+        if(miercoles instanceof Perro){
+            Perro miercoles1 = (Perro) miercoles;
+            ((Perro) miercoles).marcarTerritorio();
+
+        }
+
+    }
+}
